6d0d27a0cba5935be705a877107a75b198a91f98
AlexanderMac <amatsibarov@gmail.com>
2017-04-21 21:54:27 +0700

    improve code formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { AppComponent }  from './app.component';
 
 @NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule
-    ],
-    bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,10 +1,10 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode }         from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
 if (process.env.APP_ENV === 'production') {
-    enableProdMode();
+  enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
