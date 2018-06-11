361b058b1bb8abaa7361d4e6c78f14e4dd3d2ce4
AlexanderMac <amatsibarov@gmail.com>
2018-06-11 15:03:41 +0300

    remove old code

--- src/app/vendor.ts
+++ src/app/vendor.ts
@@ -10,12 +10,3 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'spinkit/css/spinkit.css';
 import 'toastr';
 import 'toastr/build/toastr.css';
-
-// TODO:
-// Other vendors for example jQuery, Lodash or Bootstrap
-// You can import js, ts, css, sass, ...
-/*
-import 'font-awesome/css/font-awesome.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap';
-*/
